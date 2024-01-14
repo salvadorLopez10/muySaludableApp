a0b07c1a19a40fbcea4a03a90003f602908c315a
Salvador Lopez <salvador.lopez@tactos.com.mx>
2024-01-14 14:45:17 -0600

    Nuevo diseño para el resumen del plan elegido

--- src/views/resumeChoosenPlan/ResumeChoosenPlanScreen.tsx
+++ src/views/resumeChoosenPlan/ResumeChoosenPlanScreen.tsx
@@ -54,20 +54,22 @@ export const ResumeChoosenPlanScreen = ({navigation}: Props) => {
 
   return (
     <View style={styles.container}>
-      <Image
+      {/* <Image
         source={require("../../../assets/BackGroundFresas.png")}
         style={styles.imageBackground}
-      />
+      /> */}
 
       {/* Título */}
       <View style={styles.tituloContainer}>
-        <Text style={styles.tituloText}>RESUMEN DEL PLAN ELEGIDO </Text>
-        <Text style={styles.contentTitulo}>{selectedPlan.nombre}</Text>
-        <Text style={styles.pricePlan}>${selectedPlan.precio}</Text>
+        <Text style={styles.tituloText}>RESUMEN DEL PLAN </Text>
       </View>
 
       {/* Sección de características */}
       <View style={styles.caracteristicasContainer}>
+        <Text style={styles.contentTitulo}>{selectedPlan.nombre}</Text>
+        <View style={styles.containerPrice}>
+          <Text style={styles.pricePlan}>${selectedPlan.precio}</Text>
+        </View>
         {selectedPlan?.descripcion_detallada.split("\n").map((linea, index) => (
           <View key={index} style={styles.itemContainer}>
             <Text style={styles.bullet}>•</Text>
@@ -83,16 +85,19 @@ export const ResumeChoosenPlanScreen = ({navigation}: Props) => {
       </View>
 
       {/* Sección email*/}
-      <View style={styles.overlay}>
-        <Text style={styles.etiqueta}>Ingresa tu correo electrónico</Text>
-        <TextInput style={styles.input} placeholder="Correo electrónico" value={email} onChangeText={(text)=> onChange('email', text)} />
+      <View style={styles.containerEmail}>
+        <Text style={styles.labelEmail}>Ingresa tu correo electrónico</Text>
+        <TextInput
+          style={styles.inputEmail}
+          placeholder="Correo electrónico"
+          value={email}
+          onChangeText={(text) => onChange("email", text)}
+        />
       </View>
 
       {/* Sección botón*/}
       <View style={styles.containerBoton}>
-        <TouchableOpacity style={styles.boton}
-          onPress={ onConfirmPlan }
-        >
+        <TouchableOpacity style={styles.boton} onPress={onConfirmPlan}>
           <Text style={styles.textoBoton}>Confirmar y proceder al pago</Text>
         </TouchableOpacity>
       </View>
@@ -110,40 +115,52 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   tituloContainer: {
-    position: "absolute",
+    //position: "absolute",
     alignSelf: "center",
-    top: "10%",
+    //top:50,
+    marginTop: "15%",
   },
   tituloText: {
-    color: "white",
+    color: "#326807",
     fontSize: 20,
     fontWeight: "bold",
   },
   contentTitulo: {
-    color: "#0948A6",
-    top: 20,
+    color: "#55851F",
+    //top: 20,
     fontSize: 18,
     textAlign: "center",
     fontWeight: "bold",
   },
+  containerPrice: {
+    backgroundColor: "rgba(85, 133, 31, 0.7)",
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 10,
+    marginLeft: 40,
+    marginRight: 50,
+    marginBottom: 20,
+  },
   pricePlan: {
     color: "#fff",
-    backgroundColor: "rgba(0, 0, 255, 0.5)",
     borderRadius: 20,
-    top: 40,
-    fontSize: 24,
     textAlign: "center",
+    //top: 40,
+    fontSize: 24,
     fontWeight: "bold",
-    
   },
-  containerBullet: {
-    padding: 10,
-  },
+
   caracteristicasContainer: {
-    position: "absolute",
+    backgroundColor: "#FCFDBD",
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#55851F",
+    //position: "absolute",
     alignSelf: "center",
-    top: "25%",
-    padding: 15
+    marginTop: "3%",
+    padding: 10,
   },
   itemContainer: {
     flexDirection: "row",
@@ -151,43 +168,45 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   bullet: {
-    color: "#fff",
+    color: "#55851F",
     fontSize: 20,
     marginRight: 5,
   },
   texto: {
     fontSize: 15,
     fontWeight: "bold",
-    color: "#fff",
+    color: "#55851F",
   },
   bottomContainer: {
-    position: "absolute",
+    position: "relative",
     alignSelf: "center",
-    top: "50%",
+    //top:70
+    marginTop: "2%",
   },
   fechaVigencia: {
-    color: "#0948A6",
-    top: 25,
+    color: "#55851F",
+    top: 10,
     fontSize: 20,
     textAlign: "center",
     fontWeight: "bold",
   },
-  overlay: {
-    position: "absolute",
-    top: "62%",
+  containerEmail: {
+    //position: "absolute",
+    //top: "65%",
+    marginTop: 15,
     alignSelf: "center",
     width: "70%",
-    backgroundColor: "rgba(0, 0, 255, 0.5)",
-    padding: 20,
+    backgroundColor: "rgba(85, 133, 31, 0.7)",
+    padding: 10,
     borderRadius: 10,
     alignItems: "center",
   },
-  etiqueta: {
+  labelEmail: {
     color: "white", // Color del texto de la etiqueta
     fontSize: 15,
     marginBottom: 10,
   },
-  input: {
+  inputEmail: {
     height: 40,
     width: "100%",
     backgroundColor: "white", // Color del fondo del TextInput
@@ -199,11 +218,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     alignItems: "center",
     top: "85%",
+    marginTop: 20,
     width: "100%",
   },
   boton: {
-    backgroundColor: "#0948A6",
-    padding: 10,
+    backgroundColor: "#FAA029",
+    padding: 20,
     borderRadius: 10,
     width: "70%",
     alignItems: "center",
