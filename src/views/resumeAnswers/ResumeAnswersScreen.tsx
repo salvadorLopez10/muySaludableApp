7d0f18d763ae1ea6e951370b5a290ee7696fc5eb
Salvador Lopez <salvador.lopez@tactos.com.mx>
2024-01-18 18:24:42 -0600

    Estilos para pantalla de resumen de respuestas

--- src/views/resumeAnswers/ResumeAnswersScreen.tsx
+++ src/views/resumeAnswers/ResumeAnswersScreen.tsx
@@ -2,13 +2,35 @@ import React from 'react'
 import { useRoute, RouteProp } from "@react-navigation/native";
 import { StackScreenProps } from '@react-navigation/stack';
 import { StyleSheet, View, Image, ImageBackground, ScrollView, Text, TouchableOpacity } from 'react-native'
+import { activityLevelSelect } from '../quiz/DataDropdown';
 
 interface Props extends StackScreenProps<any, any> {}
 
+interface foodListItem{
+  id: string;
+  label: string;
+}
+
 
 const ResumeAnswersScreen = ({route,navigation}:Props) => {
-
+    
     console.log(route.params);
+
+    const getLabelById = (id: string): string | undefined => {
+        const selectedOption = activityLevelSelect.find(option => option.id === id);
+        return selectedOption?.label;
+    };
+
+    const labelLevelActivity = getLabelById(route.params!.physicalActivity);
+
+    const handleCorrect = () => {
+        navigation.goBack()
+    }
+    const handleConfirm = () => {
+
+        console.log("confirmar")
+    };
+    
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -27,27 +49,36 @@ const ResumeAnswersScreen = ({route,navigation}:Props) => {
 
         <View style={styles.containerAnswer}>
           <ScrollView>
-            <Text style={styles.textAnswer}>1. { route.params!.name as any}</Text>
+            <Text style={styles.textAnswer}>
+              1. {route.params!.name as any}
+            </Text>
             <Text style={styles.textAnswer}>2. {route.params!.age} años</Text>
             <Text style={styles.textAnswer}>3. {route.params!.height} cm</Text>
             <Text style={styles.textAnswer}>4. {route.params!.weight} kg</Text>
             <Text style={styles.textAnswer}>5. {route.params!.gender}</Text>
-            <Text style={styles.textAnswer}>6. {route.params!.physicalActivity}</Text>
+            <Text style={styles.textAnswer}>6. {labelLevelActivity}</Text>
             <Text style={styles.textAnswer}>7. {route.params!.dietType}</Text>
-            <Text style={styles.textAnswer}>8. {route.params!.dietType}</Text>
+            <Text style={styles.textAnswer}>
+              8.{" "}
+              {route
+                .params!.foodAvoidListFiltered.map(
+                  (alimento: foodListItem) => alimento.label
+                )
+                .join(", ")}
+            </Text>
             <Text style={styles.textAnswer}>9. {route.params!.goal}</Text>
-            <Text style={styles.textAnswer}>10. {route.params!.stateMexico}</Text>
+            <Text style={styles.textAnswer}>
+              10. {route.params!.stateMexico}
+            </Text>
           </ScrollView>
         </View>
 
         <View style={styles.containerButtons}>
-          <TouchableOpacity style={styles.correctButton}>
-            <Text style={{ color: "white", fontWeight: "bold" }}>
-              Corregir
-            </Text>
+          <TouchableOpacity style={styles.correctButton} onPress={ handleCorrect }>
+            <Text style={{ color: "white", fontWeight: "bold" }}>Corregir</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.confirmButton}>
+          <TouchableOpacity style={styles.confirmButton} onPress={ handleConfirm }>
             <Text style={{ color: "white", fontWeight: "bold" }}>
               Confirmar
             </Text>
@@ -92,13 +123,14 @@ const styles = StyleSheet.create({
   },
   containerAnswer: {
     //backgroundColor: "blue",
-    width: "90%",
+    width: "70%",
     height: "50%",
     alignItems: "center",
   },
   textAnswer: {
     fontWeight: "bold",
     margin: 10,
+    flexWrap:"wrap"
   },
   containerButtons: {
     flexDirection: "row",
