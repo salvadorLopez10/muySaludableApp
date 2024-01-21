09f7628a4cc892583c80a0a35a55aee1d2f1a22b
Salvador Lopez <salvador.lopez@tactos.com.mx>
2024-01-21 00:38:24 -0600

    Diseño de pantalla para perfil de usuario

--- src/navigator/LateralMenu.tsx
+++ src/navigator/LateralMenu.tsx
@@ -1,14 +1,86 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView } from "@react-navigation/drawer";
 import { UserProfileScreen } from '../views/userProfile/UserProfileScreen';
 import MainMenuScreen from "../views/mainMenu/MainMenuScreen";
+import { Image, Text } from "react-native";
+import { View } from "react-native";
+import { StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 export const LateralMenu = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="UserProfileScreen" component={UserProfileScreen} />
-      <Drawer.Screen name="MainMenuScreen" component={MainMenuScreen} />
+    <Drawer.Navigator
+        drawerContent={ (props) => <InternalMenu { ...props }/> }
+    >
+      <Drawer.Screen
+        name="UserProfileScreen"
+        options={{ title: "Perfil del Usuario" }}
+        component={UserProfileScreen}
+      />
+      <Drawer.Screen
+        name="MainMenuScreen"
+        options={{ title: "Plan Alimenticio" }}
+        component={MainMenuScreen}
+      />
     </Drawer.Navigator>
   );
 }
+
+const InternalMenu = ({navigation}: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView>
+        {/* Estilos del avatar */}
+        <View style={styles.containerAvatar}>
+            <Image
+            source={ require("../../assets/logoMuySaludable.png") }
+            style={styles.avatar}
+            />
+        </View>
+
+      {/* Opciones del menú */}
+      <View style={styles.menuContainer}>
+        <TouchableOpacity style={styles.menuOpcionContainer}
+            onPress={() => navigation.navigate("UserProfileScreen")}
+        >
+            <Text style={styles.menuTexto}>Perfil de Usuario</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuOpcionContainer}
+            onPress={() => navigation.navigate("MainMenuScreen")}
+        >
+            <Text style={styles.menuTexto}>Plan Alimenticio</Text>
+        </TouchableOpacity>
+      </View>
+    
+
+    </DrawerContentScrollView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  containerAvatar: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+  menuContainer: {
+    marginVertical: 30,
+    marginHorizontal: 50,
+  },
+  menuOpcionContainer:{
+   marginVertical:20 
+  },
+  menuTexto: {
+    fontSize: 18,
+  },
+});
--- src/views/userProfile/UserProfileScreen.tsx
+++ src/views/userProfile/UserProfileScreen.tsx
@@ -1,10 +1,208 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet } from 'react-native';
+import { Text, View, ScrollView,Image, TouchableOpacity } from "react-native";
 
 export const UserProfileScreen = () => {
+
+    const handleDeleteAccount = () => {
+      console.log("SEGURO QUE DESEAS BORRAR TU CUENTA?");
+    };
+
   return (
-    <View>
-        <Text>USER PROFILE</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../../../assets/logoMuySaludable.png")}
+          style={styles.logoImage}
+        />
+      </View>
+
+       <ScrollView>
+            <View style={styles.generalInfoContainer}>
+                <Text style={styles.generalInfoTitleText}>SALVADOR LOPEZ BALLEZA</Text>
+                <Text style={styles.generalInfoSubtitleText}>jslb_cafcb10@hotmail.com</Text>
+            </View>
+
+            <View style={styles.infoUserContainer}>
+                <View style={styles.dataUserBox}>
+                    <Text style={styles.dataUser}>31 años</Text>
+                </View>
+                <View style={styles.dataUserBox}>
+                    <Text style={styles.dataUser}>183 cm</Text>
+                </View>
+                <View style={styles.dataUserBox}>
+                    <Text style={styles.dataUser}>110 kg</Text>
+                </View>
+            </View>
+
+            <View style={styles.statusSuscriptionContainer}>
+                <Text style={styles.suscriptionText}>SUSCRIPCIÓN ACTIVA</Text>
+                <Text style={styles.suscriptionPlan}>PLAN PREMIUM</Text>
+            </View>
+
+            <View style={styles.dataUserContainer}>
+                <View style={styles.datosInfoBox}>
+                    <Text style={styles.datosInfoText}>OBJETIVO:</Text>
+                    <Text style={styles.datosInfoText}>Eliminar grasa</Text>
+                </View>
+            </View>
+
+            <View style={styles.dataUserContainer}>
+                <View style={styles.datosInfoBox}>
+                    <Text style={styles.datosInfoText}>TIPO DE DIETA:</Text>
+                    <Text style={styles.datosInfoText}>Todo tipo de alimentos</Text>
+                </View>
+            </View>
+
+            <View style={styles.dataUserContainer}>
+                <View style={styles.datosInfoBox}>
+                    <Text style={styles.datosInfoText}>NIVEL DE ACTIVIDAD FíSICA:</Text>
+                    <Text style={styles.datosInfoText}>Ejercicio moderado</Text>
+                </View>
+            </View>
+            <View style={styles.dataUserContainer}>
+                <View style={styles.datosInfoBox}>
+                    <Text style={styles.datosInfoText}>ALIMENTOS A EVITAR:</Text>
+                    <Text style={styles.datosInfoText}>JAMÓN</Text>
+                </View>
+            </View>
+
+            <View style={styles.footer}>
+                <View style={styles.buttonDeleteContainer}>
+                    <TouchableOpacity style={styles.buttonDelete} onPress={ handleDeleteAccount }>
+                        <Text style={{ color: "white", fontWeight: "bold" }}>Eliminar cuenta</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+       </ScrollView>
+
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    //justifyContent: "center",
+  },
+  logoContainer: {
+    marginVertical: 20,
+    alignSelf: "center",
+    //top: "10%",
+  },
+  logoImage: {
+    width: 100,
+    height: 100,
+  },
+  generalInfoContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  generalInfoTitleText: {
+    fontSize: 20,
+    color: "#326807",
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  generalInfoSubtitleText: {
+    fontSize: 16,
+    color: "#326807",
+    fontWeight: "bold",
+  },
+  infoUserContainer: {
+    flexDirection: "row",
+    width: "80%",
+    top: 10,
+    alignItems: "center",
+    margin: "10%",
+    marginVertical: 10,
+  },
+  dataUserBox: {
+    width: "33%",
+    alignItems: "center",
+    backgroundColor: "#faa029",
+    borderRadius: 10,
+    padding: 5,
+    top: 5,
+    margin: 3,
+    marginVertical: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  dataUser: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 5,
+    color: "#ffff",
+  },
+  statusSuscriptionContainer: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  suscriptionText: {
+    fontSize: 18,
+    color: "#326807",
+    fontWeight: "bold",
+    marginVertical: 3,
+  },
+  suscriptionPlan: {
+    fontSize: 18,
+    color: "#326807",
+    fontWeight: "bold",
+    marginVertical: 3,
+    textDecorationLine: "underline",
+  },
+  dataUserContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  datosInfoBox: {
+    width: "80%",
+    alignItems: "center",
+    backgroundColor: "#faa029",
+    borderRadius: 10,
+    padding: 5,
+    top: 5,
+    margin: 3,
+    marginVertical: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  datosInfoText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 5,
+    color: "#ffff",
+  },
+  footer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonDeleteContainer: {
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonDelete: {
+    padding: 10,
+    width: "100%",
+    backgroundColor: "rgba(224, 26, 0, 0.8)",
+    alignItems: "center",
+    //marginTop: 10,
+    borderRadius: 15,
+    marginVertical: 10,
+  },
+});
